eaea72930f03119d78f11d7d10ad6f79730cc940
brahim-medkour <medkourbrahim08@gmail.com>
2025-09-17 12:13:55 +0100

    on crée un variable et condition

--- script.js
+++ script.js
@@ -2,3 +2,37 @@ let nombre1 = 10;
 let nombre2 = 5;
 let somme = nombre1 + nombre2;
 console.log("La somme est  : " + somme);
+
+
+
+
+
+let age = 18;
+if (age >= 18) {
+    console.log("Vous êtes majeur.");
+} else {
+    console.log("Vous êtes mineur.");
+}   
+
+
+
+let jeure = "lundi";
+switch (jeure) {
+    case "lundi":
+        console.log("Aujourd'hui, c'est lundi.");
+        break;
+    case "mardi":
+        console.log("Aujourd'hui, c'est mardi.");
+        break;
+}
+
+
+let ages = 20;
+
+if (ages < 18) {
+  console.log("Vous êtes mineur.");
+} else if (ages === 18) {
+  console.log("Vous venez d’atteindre la majorité !");
+} else {
+  console.log("Vous êtes majeur.");
+}
